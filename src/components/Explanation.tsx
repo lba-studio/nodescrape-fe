0dcd58707196f46ae962a663cc775723b2294cab
verzac <benjamin.tanone@gmail.com>
2020-03-17 01:20:31 +1100

    edit explanation content

--- src/components/Explanation.tsx
+++ src/components/Explanation.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Button, Box, Dialog, DialogTitle, DialogContent, DialogActions } from "@material-ui/core";
+import { Button, Box, Dialog, DialogTitle, DialogContent, DialogActions, Typography } from "@material-ui/core";
+import PageSection from "./PageSection";
 
 export default function () {
   const [dialogOpen, setDialogOpen] = React.useState(false);
@@ -11,15 +12,36 @@ export default function () {
     <Dialog onClose={onClose} open={dialogOpen}>
       <DialogTitle>How does this work?</DialogTitle>
       <DialogContent>
+        <PageSection>
+          <Typography variant="h3">What do the scores mean?</Typography>
+          <b>Each news source is given a score from -1 to 1.</b> These scores are obtained by doing an automated sentiment analysis on the source's most recent 100 text articles and averaging each articles' scores.
           <ul>
-            <li><b>Scores are from -1 to 1</b>, with -1 implying that all the text in that news source only contains sentimentally negative words, and 1 implying that the text only contains positive words.</li>
+            <li>1 = implies that the news source contains only sentimentally positive articles. <i>For example: "I love ice creams because they are really delicious!"</i></li>
+            <li>0 = implies that the news source contains only sentimentally neutral articles. <i>For example: "1 + 1 is 2."</i></li>
+            <li>-1 = implies that the news source contains only sentimentally negative articles. <i>For example: "I really hate life. Go f*** yourself."</i></li>
+          </ul>
+        </PageSection>
+        <PageSection>
+          <Typography variant="h3">How do we come up with the score for a news source?</Typography>
+          <ul>
             <li>A scraper retrieves news articles from various sources (English-only, for now).</li>
-            <li>Those news articles are then fed into our sentiment analysis pipeline.</li>
-            <li>The text in those news articles are broken down into "tokens" (i.e. individual words).</li>
-            <li>The words are run against a "lexicon" (specifically AFINN, for now; see roadmap); things that contain words and their properties (in this case, our sentiment polarity score).</li>
+            <li>Those news articles are then fed into our sentiment analysis pipeline, which spits out this score.</li>
             <li>The scores of each tokens/words in the news articles are then averaged for their respective news sources.</li>
           </ul>
+        </PageSection>
+        <PageSection>
+          <Typography variant="h3">Sentiment analysis pipeline? What's that?</Typography>
+          <p>It sounds fancy, but it really isn't.</p>
+          <p>We are currently using Amazon Comprehend, which is Amazon's natural language processing (NLP) service. In essence, it's sort of like the system that powers Amazon Echo/Google Home in the sense that it uses machine learning to understand what someone is trying to say.</p>
+        </PageSection>
+        <PageSection>
+          <Typography variant="h3">Wait, so can the score be biased?</Typography>
+          <p>Of course! Sentiments are always subjective; it is literally to the eye of the beholden. Words mean differently in different contexts and cultures, so it is ultimately impossible to come up with an objective score.</p>
+          <p>However, by using hosted services (such as Amazon Comprehend), we hope to leverage the immense dataset that they have, which should yield a more accurate, less-biased result.</p>
+        </PageSection>
+        <PageSection>
           <i>Keep in mind that this is still under active development, and can change at anytime! Visit the <a target="_blank" rel="noopener noreferrer" href="https://github.com/verzac/nodescrape-fe">project's GitHub</a> to ask any questions or to give any feedback.</i>
+        </PageSection>
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose}>OK! Take me back!</Button>
