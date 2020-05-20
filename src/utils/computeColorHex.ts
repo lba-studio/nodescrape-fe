8423039098232d04383ad098c9af5ab447b3a512
verzac <benjamin.tanone@gmail.com>
2020-05-20 21:55:53 +1000

    changed computeColorHex to use a scale from red -> orange -> green

--- src/utils/computeColorHex.ts
+++ src/utils/computeColorHex.ts
@@ -1,22 +1,19 @@
 /**
  * computes the color hex based on the passed in ratio (so something like #0101ff)
+ * thanks to https://gist.github.com/mlocati/7210513 
  * @param ratio a number from -1 to 1
  */
 export default function(ratio: number): string {
-  if (ratio < -1 || ratio > 1) {
-    throw new Error(`Cannot compute hex of ratio that is smaller than 1 or larger than 1. Ratio: ${ratio}`);
-  }
-  // so this is out of 100%
-  let outputHex: string = '';
-  let hex = '';
-  if (ratio > 0) {
-    hex = (128 - Math.min(64, Math.floor(64 * ratio * 2)) - 63).toString(16).padStart(2, '0');
-    outputHex = `#${hex}ff${hex}`;
-  } else if (ratio < 0) {
-    hex = (128 - Math.min(128, Math.floor(64 * Math.abs(ratio) * 4))).toString(16).padStart(2, '0');
-    outputHex = `#ff${hex}${hex}`
-  } else {
-    outputHex = '#ffffff'
-  }
-  return outputHex;
+  const perc = (ratio + 1) / 2 * 100
+  var r, g, b = 0;
+	if(perc < 50) {
+		r = 255;
+		g = Math.round(5.1 * perc);
+	}
+	else {
+		g = 255;
+		r = Math.round(510 - 5.10 * perc);
+	}
+	var h = r * 0x10000 + g * 0x100 + b * 0x1;
+	return '#' + ('000000' + h.toString(16)).slice(-6);
 }
