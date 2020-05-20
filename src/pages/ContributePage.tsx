0414aa80390396d1ced2ba105e57595e5c33ba52
verzac <benjamin.tanone@gmail.com>
2020-05-20 18:41:26 +1000

    update dependencies and fixed lint issues in ContributePage

--- src/pages/ContributePage.tsx
+++ src/pages/ContributePage.tsx
@@ -8,32 +8,28 @@ const ContributePage = () => {
     <PageSection>
       <Typography variant="h1" id="about-us">About Us</Typography>
       <Typography>
-        <p>
-          The news is an integral part of our lives.
-          We listen to news outlets in order to obtain the latest information regarding events which are occurring around us.
-          In essence, watching the news would allow us to form a picture of reality.
-          For some of us, it is literally our virtual doorway to the outside world.
-        </p>
-        <p>
-          However, we've never considered how the news would make us feel.
-          With the ongoing battle to grab your attention, media outlets often opt to connect emotionally with you.
-          Sensationalist news are everywhere; ranging from scathing coverage of royal weddings to glowing reviews of your country's immigration policy. Everyone aims to appeal to you and other readers like you.
-        </p>
-        <p>
-          <b>This is why we've built {siteTitle}.</b> We want to make sure that you have the tools to be aware of how the news could affect you mentally and emotionally.
-        </p>
+        The news is an integral part of our lives.
+        We listen to news outlets in order to obtain the latest information regarding events which are occurring around us.
+        In essence, watching the news would allow us to form a picture of reality.
+        For some of us, it is literally our virtual doorway to the outside world.
+      </Typography>
+      <Typography>
+        However, we've never considered how the news would make us feel.
+        With the ongoing battle to grab your attention, media outlets often opt to connect emotionally with you.
+        Sensationalist news are everywhere; ranging from scathing coverage of royal weddings to glowing reviews of your country's immigration policy. Everyone aims to appeal to you and other readers like you.
+      </Typography>
+      <Typography>
+        <b>This is why we've built {siteTitle}.</b> We want to make sure that you have the tools to be aware of how the news could affect you mentally and emotionally.
       </Typography>
     </PageSection>
     <PageSection>
       <Typography variant="h2">Interested in contributing?</Typography>
       <Typography>
-        <p>
-          We'd love anyone who's interested in this project to contribute!
-        </p>
-        <p>
-          We've set up a <Link href="https://trello.com/b/phaKT6at/newswatch-board" target="_blank" rel="noopener noreferrer">Trello board</Link> to track where we're at with the project and what kind of features we want to build out.
-          If this sounds like your thing, get in touch with us through <Link target="_blank" rel="noopener noreferrer" href="https://about.benjamintanone.com">Ben's contact form</Link>.
-        </p>
+        We'd love anyone who's interested in this project to contribute!
+        </Typography>
+      <Typography>
+        We've set up a <Link href="https://trello.com/b/phaKT6at/newswatch-board" target="_blank" rel="noopener noreferrer">Trello board</Link> to track where we're at with the project and what kind of features we want to build out.
+        If this sounds like your thing, get in touch with us through <Link target="_blank" rel="noopener noreferrer" href="https://about.benjamintanone.com">Ben's contact form</Link>.
       </Typography>
     </PageSection>
   </>;
